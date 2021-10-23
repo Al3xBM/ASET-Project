f88022542c81af617b8c37579ed36e766a975144
Alexandru Balan <61700567+Al3xBM@users.noreply.github.com>
2021-10-23 16:07:16 +0300

    Created solution

---
+++ VisualTwitter/UserService/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace UserService.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/v1/users")]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] object dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] object dto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
